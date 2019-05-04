3973ae5e316a0f533262cf30f40381bba177cceb
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-05-04 10:24:00 +0200

    Implement registry

--- TranslatorRegistry.cs
+++ TranslatorRegistry.cs
@@ -6,9 +6,16 @@ namespace MapsetSnapshotter
 {
     public class TranslatorRegistry
     {
+        private static List<DiffTranslator> translators = new List<DiffTranslator>();
+
+        public static void RegisterTranslator(DiffTranslator aTranslator)
+        {
+            translators.Add(aTranslator);
+        }
+
         public static IEnumerable<DiffTranslator> GetTranslators()
         {
-
+            return new List<DiffTranslator>(translators);
         }
     }
 }
