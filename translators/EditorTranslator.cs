564a258ddf3973f3fb12b89827fbecd611234139
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-05-04 15:36:39 +0200

    Add editor translator

---
+++ translators/EditorTranslator.cs
@@ -0,0 +1,28 @@
+﻿using MapsetSnapshotter.objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapsetSnapshotter.translators
+{
+    public class EditorTranslator : DiffTranslator
+    {
+        public override string Section { get => "Editor"; }
+
+        public override IEnumerable<DiffInstance> Translate(IEnumerable<DiffInstance> aDiffs)
+        {
+            foreach (DiffInstance aDiff in Snapshotter.TranslateSettings(Section, aDiffs, TranslateKey))
+                yield return aDiff;
+        }
+
+        private static string TranslateKey(string aKey)
+        {
+            return
+                aKey == "DistanceSpacing" ? "Distance spacing" :
+                aKey == "BeatDivisor"     ? "Beat snap divisor" :
+                aKey == "GridSize"        ? "Grid size" :
+                aKey == "TimelineZoom"    ? "Timeline zoom" :
+                aKey;
+        }
+    }
+}
