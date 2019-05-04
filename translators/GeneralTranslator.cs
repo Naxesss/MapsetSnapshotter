fe8828018fa0771d1c8d443d25f5351a0a1e7678
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-05-04 11:09:21 +0200

    Apply difftranslator abstraction

--- translators/GeneralTranslator.cs
+++ translators/GeneralTranslator.cs
@@ -5,33 +5,30 @@ using System.Text;
 
 namespace MapsetSnapshotter.translators
 {
-    public class GeneralTranslator
+    public class GeneralTranslator : DiffTranslator
     {
-        public class TranslatorGeneral : DiffTranslator
+        public override string Section { get => "General"; }
+
+        public override IEnumerable<DiffInstance> Difference(IEnumerable<DiffInstance> aDiffs)
         {
-            public override string Section { get => "General"; }
+            foreach (DiffInstance aDiff in Snapshotter.TranslateSettings(Section, aDiffs, TranslateKey))
+                yield return aDiff;
+        }
 
-            public static IEnumerable<DiffInstance> Execute(IEnumerable<DiffInstance> aDiffs)
-            {
-                foreach (DiffInstance aDiff in Snapshotter.TranslateSettings(Section, aDiffs, TranslateKey))
-                    yield return aDiff;
-            }
-
-            private static string TranslateKey(string aKey)
-            {
-                return aKey == "AudioFilename" ? "Audio filename" :
-                        aKey == "AudioLeadIn" ? "Audio lead-in" :
-                        aKey == "PreviewTime" ? "Preview time" :
-                        aKey == "SampleSet" ? "Default sample set" :
-                        aKey == "StackLeniency" ? "Stack leniency" :
-                        aKey == "LetterboxInBreaks" ? "Letterboxing in breaks" :
-                        aKey == "WidescreenStoryboard" ? "Widescreen storyboard" :
-                        aKey == "StoryFireInFront" ? "Storyboard in front of combo fire" :
-                        aKey == "SpecialStyle" ? "Special N+1 style" :
-                        aKey == "UseSkinSprites" ? "Use skin sprites in storyboard" :
-                        aKey == "EpilepsyWarning" ? "Epilepsy warning"
-                                                        : aKey;
-            }
+        private static string TranslateKey(string aKey)
+        {
+            return aKey == "AudioFilename" ? "Audio filename" :
+                    aKey == "AudioLeadIn" ? "Audio lead-in" :
+                    aKey == "PreviewTime" ? "Preview time" :
+                    aKey == "SampleSet" ? "Default sample set" :
+                    aKey == "StackLeniency" ? "Stack leniency" :
+                    aKey == "LetterboxInBreaks" ? "Letterboxing in breaks" :
+                    aKey == "WidescreenStoryboard" ? "Widescreen storyboard" :
+                    aKey == "StoryFireInFront" ? "Storyboard in front of combo fire" :
+                    aKey == "SpecialStyle" ? "Special N+1 style" :
+                    aKey == "UseSkinSprites" ? "Use skin sprites in storyboard" :
+                    aKey == "EpilepsyWarning" ? "Epilepsy warning"
+                                                    : aKey;
         }
     }
 }
