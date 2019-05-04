c9802257f1b91d37321004bf164c0b930db4cc00
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-05-04 15:36:20 +0200

    Add difficulty translator

---
+++ translators/DifficultyTranslator.cs
@@ -0,0 +1,28 @@
+﻿using MapsetSnapshotter.objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapsetSnapshotter.translators
+{
+    public class DifficultyTranslator : DiffTranslator
+    {
+        public override string Section { get => "Difficulty"; }
+
+        public override IEnumerable<DiffInstance> Translate(IEnumerable<DiffInstance> aDiffs)
+        {
+            foreach (DiffInstance aDiff in Snapshotter.TranslateSettings(Section, aDiffs, TranslateKey))
+                yield return aDiff;
+        }
+
+        private static string TranslateKey(string aKey)
+        {
+            return
+                aKey == "HPDrainRate"       ? "HP drain rate" :
+                aKey == "CircleSize"        ? "Circle size" :
+                aKey == "OverallDifficulty" ? "Overall difficulty" :
+                aKey == "ApproachRate"      ? "Approach rate" :
+                aKey;
+        }
+    }
+}
