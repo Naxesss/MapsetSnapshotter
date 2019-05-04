9a948cf87af66552777c757599f6ebc68e362e0c
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-05-04 11:09:49 +0200

    Add registry initalization

--- TranslatorRegistry.cs
+++ TranslatorRegistry.cs
@@ -1,4 +1,5 @@
 ﻿using MapsetSnapshotter.objects;
+using MapsetSnapshotter.translators;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,17 @@ namespace MapsetSnapshotter
     public class TranslatorRegistry
     {
         private static List<DiffTranslator> translators = new List<DiffTranslator>();
+        private static bool initialized;
+
+        public static void InitalizeTranslators()
+        {
+            if (initialized)
+                return;
+
+            RegisterTranslator(new GeneralTranslator());
+
+            initialized = true;
+        }
 
         public static void RegisterTranslator(DiffTranslator aTranslator)
         {
@@ -16,7 +28,9 @@ namespace MapsetSnapshotter
 
         public static IEnumerable<DiffTranslator> GetTranslators()
         {
+            InitalizeTranslators();
             return new List<DiffTranslator>(translators);
         }
+
     }
 }
