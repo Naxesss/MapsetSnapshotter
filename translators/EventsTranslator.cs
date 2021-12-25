87f7724d2f624ad9c03ab5017e4ba8d91166469e
Naxess <30292137+Naxesss@users.noreply.github.com>
2021-12-25 08:20:40 +0100

    Remove unused import

--- translators/EventsTranslator.cs
+++ translators/EventsTranslator.cs
@@ -4,7 +4,6 @@ using MapsetSnapshotter.objects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using MathNet.Numerics;
 using static MapsetSnapshotter.Snapshotter;
 
